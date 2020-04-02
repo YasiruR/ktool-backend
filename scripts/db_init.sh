1b8b485b08730410ce55eb0b0caf9ea5a0521ddd
Yasi <yasiru@pickme.lk>
2020-04-02 22:47:42 +0530

    implemented adding all brokers for a given cluster to database on create

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#pulling mysql from docker and executing the image
-echo initialization of the database... 
-#sudo su
-#sudo docker pull mysql/mysql-server
-#echo docker image is pulled and stored...
-#sudo docker run --name=mysql1 -d mysql/mysql-server:tag
-#echo docker mysql image is running...
+#Need to run this script as a root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (with sudo command)"
+  exit
+fi
 
+echo initialization of the database... 
 #downloading locally
-sudo yum update
-sudo yum install wget -y
+yum update
+yum install wget -y
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 echo mysql package is downloaded...
-sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
-sudo yum update
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum update
 echo updating yum...
-sudo yum install mysql-server -y
+yum install mysql-server -y
 echo installing mysql-server...
-sudo systemctl start mysqld
+systemctl start mysqld
 echo starting mysql-server...
 
 #creating sql user and logging in
@@ -27,23 +26,22 @@ echo Username :
 read -r username
 echo Password :
 read -r password
-mysql -u root -p
-\n
-GRANT ALL PRIVILEGES ON *.* TO "$username"@'localhost' IDENTIFIED BY "$password";
+echo Please provide the root password - default procedure is to skip this
+mysql -u root <<MYSQL_SCRIPT
+GRANT ALL PRIVILEGES ON *.* TO "$username"@'localhost'IDENTIFIED BY "$password";
 \q
-mysql -u "$username" -p
-echo "$password"
-echo mysql user is created with username:$username
+MYSQL_SCRIPT
 
+#logging into the user
+mysql -u "$username" -p"$password" <<INIT_SCRIPT
 #creating the db
 CREATE DATABASE kdb;
 USE kdb;
 CREATE TABLE user (id int(10) not null primary key auto_increment, username varchar(30) not null unique, token varchar(30) unique, access_level varchar(10), password varchar(100), first_name varchar(30), last_name varchar(30), email varchar(50));
 CREATE TABLE cluster (id int(10) not null primary key auto_increment, cluster_name varchar(30) unique, kafka_version varchar(10), active_controllers int(10));
 CREATE TABLE broker (id int(10) not null primary key auto_increment, host varchar(100) unique, port int(10), created_at datetime, cluster_id int(10) not null);
-echo Tables created :
-DESCRIBE user;
-DESCRIBE cluster;
-DESCRIBE broker;
 \q
+INIT_SCRIPT
+
+echo kdb database initialized successfully with user, cluster and broker tables
 
---
+++ scripts/upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ..
+go build
+scp ktool-backend 35.213.187.51:/opt/ktool-backend
